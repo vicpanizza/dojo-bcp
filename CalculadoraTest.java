ea014ec73d1183dd9ba4c1944ee55c5cd54b1546
vicpanizza <victor.panizza@gmail.com>
2017-11-02 11:54:12 -0500

    incluir el before en la clase de prueba

--- CalculadoraTest.java
+++ CalculadoraTest.java
@@ -1,10 +1,15 @@
 package com.bcp.bancamovil.demo;
 
+import org.junit.Before;
 import org.junit.Test;
 import junit.framework.Assert;
 
 public class CalculadoraTest{
 	
+	@Before
+	public void init() {
+		
+	}
 
 	@Test
 	public void testSuma(){
